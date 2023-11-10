e3a90d0c932e418f918b3f37f3464a2839badabd
Anurag Rajput <arajput3000@gmail.com>
2023-11-10 23:03:15 +0530

    added chatting

--- message.php
+++ message.php
@@ -25,14 +25,44 @@
                     echo "<p>".$row['user1']." : ".$row['message']."</p>";
                 }
             }
+
+            mysqli_close($conn);
         }
         ?>
     </div>
 
+    <div id="inputmessage">
+        <form method="post" action="message.php">
+            <textarea name="message" id="message" cols="30" rows="10"><?php if (isset($_POST['message'])) echo $_POST['message']; ?></textarea>
+            <input type="hidden" name="user1" value="<?php echo $user1; ?>">
+            <input type="hidden" name="user2" value="<?php echo $user2; ?>">
+            <button type="submit">send</button>
+        </form>
+    </div>
+
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['message']) && !empty($_POST['message'])) {
+        $conn = mysqli_connect("localhost", "root", "anurag", "anurag");
+        $message = $_POST['message'];
+        $user1 = $_POST['user1'];
+        $user2 = $_POST['user2'];
+
+        $query = "INSERT INTO messages VALUES ('$user1', '$user2', '$message', NOW())";
+        mysqli_query($conn, $query);
+
+        // Redirect to refresh the page and show updated messages
+        header("Location: message.php?user1=$user1&user2=$user2");
+        exit();
+    }
+    ?>
+
     <script>
         setTimeout(function(){
-            window.location.href = "message.php?user1=<?php echo $user1; ?>&user2=<?php echo $user2; ?>";
-        }, 5000); // Refresh the page after 1 second with the same user data
+            // Check if the message input is empty before refreshing the page
+            if (document.getElementById('message').value.trim() === "") {
+                window.location.href = "message.php?user1=<?php echo $user1; ?>&user2=<?php echo $user2; ?>";
+            }
+        }, 5000); // Refresh the page after 5 seconds if the message input is empty
     </script>
 </body>
 </html>
